a885de8e680a08481f4bf5940ebac7159b888088
Marina <m.gerasymova29@gmail.com>
2020-11-11 00:49:09 +0200

    Updates

---
+++ precache-manifest.431b1c0532e84682e9d1ab6c687eb490.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23e434dc196f2261e4b41ad37450943a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c85c08e9eafb0720883b",
+    "url": "./static/css/2.92730f33.chunk.css"
+  },
+  {
+    "revision": "f2349df1e3878b66d392",
+    "url": "./static/css/main.6ce43aae.chunk.css"
+  },
+  {
+    "revision": "c85c08e9eafb0720883b",
+    "url": "./static/js/2.f101576e.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "./static/js/2.f101576e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2349df1e3878b66d392",
+    "url": "./static/js/main.bdbd3e84.chunk.js"
+  },
+  {
+    "revision": "aae0386031766cdb5520",
+    "url": "./static/js/runtime-main.63136dd4.js"
+  }
+]);
